61f45df6bb28897f5dbe0ad3598512cac32db968
WriteCoin <sovtsardom@gmail.com>
2020-12-05 00:31:51 +0300

    добавлена функция проверки на принадлежность к классу

--- index.lua
+++ index.lua
@@ -5,10 +5,14 @@ function newClass()
 end
 
 function newObject(class)
-    local obj = {}
+    local obj = {__class = tostring(class)}
     setmetatable(obj, class)
     class.__index = class
     return obj
+end
+
+function instanceof(obj, class)
+    return obj.__class == tostring(class)
 end
 
 function extendedClass(parent)
